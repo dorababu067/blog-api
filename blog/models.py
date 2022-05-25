22a275a708137186384a82ff2fa99afed98f9a20
dorababu067 <dorababua67@gmail.com>
2022-05-25 16:16:34 +0530

    generic class based views added

--- blog/models.py
+++ blog/models.py
@@ -6,6 +6,7 @@ from django.db import models
 class Blog(models.Model):
     title = models.CharField(max_length=256)
     content = models.TextField()
+    category = models.CharField(max_length=256, null=True, blank=True)
 
     class Meta:
         verbose_name = "blog"
--- blog/views.py
+++ blog/views.py
@@ -1,58 +1,21 @@
-from ast import Delete
-from django.http import Http404
-
-
-from rest_framework import status
-from rest_framework.views import APIView
-from rest_framework.response import Response
+from rest_framework.generics import (
+    ListAPIView,
+    CreateAPIView,
+    RetrieveAPIView,
+    UpdateAPIView,
+    DestroyAPIView,
+)
 
 from .models import Blog
 from .serializers import BlogSerializer
 
 
-class BlogEndpoint(APIView):
-    def get(self, request):
-        # queryset will return all objects inside the blog table
-        blogs = Blog.objects.all()
-        serializer = BlogSerializer(blogs, many=True)
-        return Response(serializer.data)
-
-    def post(self, request):
-        serializer = BlogSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+class BlogEndpoint(ListAPIView, CreateAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
 
 
-class BlogDetailEndpoint(APIView):
-    def get_blog(self, id):
-        try:
-            blog = Blog.objects.get(id=id)
-            return blog
-        except Blog.DoesNotExist:
-            raise Http404
-
-    # get individual blog
-    def get(self, request, id):
-        blog = self.get_blog(id)
-        serializer = BlogSerializer(blog)
-        return Response(serializer.data)
-
-    def put(self, request, id):
-        blog = self.get_blog(id)
-
-        # Update the
-        serializer = BlogSerializer(instance=blog, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_200_OK)
-        else:
-            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, id):
-        blog = self.get_blog(id)
-
-        blog.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
+class BlogDetailEndpoint(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
+    serializer_class = BlogSerializer
+    queryset = Blog.objects.all()
+    lookup_field = "id"

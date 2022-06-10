ab02a5c1daa3e5c4e8d6426f74852250f8b9924e
dorababu067 <dorababua67@gmail.com>
2022-06-10 15:57:39 +0530

    authentication added

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,21 @@
 from django.urls import path
-from .views import BlogEndpoint, BlogDetailEndpoint
+from .views import (
+    BlogEndpoint,
+    BlogDetailEndpoint,
+    UserRegisterEndpoint,
+)
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
+
 
 urlpatterns = [
     path("blogs/", BlogEndpoint.as_view()),
     # blog detail endpoint this url will handle, retrieve, put, delete
     path("blogs/<int:id>/", BlogDetailEndpoint.as_view()),
+    path("register/", UserRegisterEndpoint.as_view()),
+    # JWTAuthentication
+    path("token/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -5,17 +5,34 @@ from rest_framework.generics import (
     UpdateAPIView,
     DestroyAPIView,
 )
+from rest_framework.permissions import IsAuthenticated
 
 from .models import Blog
-from .serializers import BlogSerializer
+from django.contrib.auth.models import User
+
+from .serializers import BlogSerializer, UserSerializer
 
 
 class BlogEndpoint(ListAPIView, CreateAPIView):
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
+    permission_classes = [IsAuthenticated]
 
 
 class BlogDetailEndpoint(RetrieveAPIView, UpdateAPIView, DestroyAPIView):
     serializer_class = BlogSerializer
     queryset = Blog.objects.all()
     lookup_field = "id"
+    permission_classes = [IsAuthenticated]
+
+
+class UserRegisterEndpoint(CreateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+    def perform_create(self, serializer):
+        if serializer.is_valid():
+            user = serializer.save()
+            user.set_password(self.request.data.get("password"))
+            # crete new user
+            user.save()
